256e5eff544c8629292aeb5bd65cdd5f8b273ef9
TheOriginalMehrdad <96524978+TheOriginalMehrdad@users.noreply.github.com>
2026-06-24 21:05:26 +0330

    Initial Commit

---
+++ Pomo/PomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoApp.swift
+//  Pomo
+//
+//  Created by Mehrdad on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
